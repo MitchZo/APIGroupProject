105bf65b28963c4299e08c2c6fa9f744eb404543
mitch <mitchell.zolynsky@gmail.com>
2019-09-04 13:15:30 -0400

    first commit

---
+++ ApiProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApiProject.Models;
+using System.Net.Http;
+
+namespace ApiProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string baseAddress = "http://www.omdbapi.com";
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult<Movie>> SearchResults(string Title, string ReleaseYear, string Rated, string Language, string Runtime, string Genre)
+        {
+
+            return View();
+        }
+    }
+}
