0d6d2bb437876ef5fc70717c83d07ef3ec1b3e03
mitch <mitchell.zolynsky@gmail.com>
2019-09-04 15:34:19 -0400

    catch up to master

--- ApiProject/Controllers/HomeController.cs
+++ ApiProject/Controllers/HomeController.cs
@@ -8,11 +8,14 @@ using ApiProject.Models;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+using System.Configuration;
 
 namespace ApiProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Context
         private readonly IConfiguration _configuration;
         public HomeController(IConfiguration configuration)
         {
@@ -26,22 +29,25 @@ namespace ApiProject.Controllers
         public async Task<ActionResult<Movie>> SearchResults(string Title, string ReleaseYear, string Rated, string Language, string Runtime, string Genre)
         {
             var ApiKey = _configuration.GetSection("AppConfiguration")["APIKeyValue"];
-
             var client = new HttpClient();
             string searchAddress = "";
             if (Title != null)
             {
-                searchAddress = $"";
+                searchAddress = $"?s={Title}&apikey={ApiKey}";
             }
             else if (ReleaseYear != null)
             {
-                searchAddress = $"";
+                searchAddress = $"?y={ReleaseYear}&apikey={ApiKey}";
             }
             client.BaseAddress = new Uri("http://www.omdbapi.com");
             var response = await client.GetAsync(searchAddress);
             var something = await response.Content.ReadAsStringAsync();
             var movies = JsonConvert.DeserializeObject<Movies>(something);
             return View(movies);
+        }
+        public async Task<ActionResult<Movie>> AddToFavs()
+        {
+            
         }
     }
 }
